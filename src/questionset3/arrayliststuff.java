6a128de5b3682a011902373b5827567bf1c08635
vinnipukh <73662383+vinnipukh@users.noreply.github.com>
2025-03-16 23:28:14 +0300

    solved questions 2,3,4

--- src/questionset3/arrayliststuff.java
+++ src/questionset3/arrayliststuff.java
@@ -9,6 +9,11 @@ public class arrayliststuff {
 
     }
 
+    /*
+    Question 1 (ArrayLists)
+ Write a function that takes an ArrayList of integers and removes any duplicate values,
+ keeping only the first occurrence.
+    */
 
     public ArrayList<Integer> removeDuplicate(ArrayList<Integer> integerArrayList){
         HashSet<Integer> seen = new HashSet<>();
@@ -22,5 +27,50 @@ public class arrayliststuff {
         }
 
         return result;
+    }
+    /*
+    Question 2 (ArrayLists)
+Create a function with two ArrayLists of integers as parameters. Merge the two lists into a
+third ArrayList without duplicates. Return the resulting ArrayList
+     */
+    public ArrayList<Integer> mergeArrayLists(ArrayList<Integer> list1,ArrayList<Integer> list2){
+        ArrayList<Integer> resultList = new ArrayList<>(list1);
+        resultList.addAll(list2);
+        removeDuplicate(list2);
+        return resultList;
+    }
+    /*
+    Question 3(ArrayLists)
+    Create a function with two ArrayLists of integers as parameters. Return an ArrayList that
+    contains integers that exist in both ArrayLists.
+     */
+    public ArrayList<Integer> sameIntegerList(ArrayList<Integer> list1,ArrayList<Integer> list2){
+        ArrayList<Integer> resultList = new ArrayList<>();
+        for(int i : list1){
+            if(list2.contains(i)){
+                resultList.add(i);
+            }
+        }
+        return resultList;
+    }
+    /*
+    Question 4(ArrayLists)
+  Write a program that first creates an integer ArrayList with integers from 0 to 100 ( [0, 1, 2,
+  …,100] ), then filters the ArrayList of integers keeping only the even numbers, and print the
+  result.
+     */
+    public void evenList(){
+        ArrayList<Integer> hundredList = new ArrayList<>();
+        ArrayList<Integer> evenList = new ArrayList<>();
+
+        for(int i = 0; i<=100; i++){
+            hundredList.add(i);
+        }
+        for(int i : hundredList){
+            if(hundredList.get(i)%2==0){
+                evenList.add(i);
+            }
+        }
+        evenList.toString();
     }
 }
